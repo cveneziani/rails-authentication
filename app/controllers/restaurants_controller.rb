81ddb198f2047064b0e4283b7ec3179d1463071a
Cecile Veneziani <contact@cecilitse.org>
2022-11-22 10:19:01 +0100

    Add skeleton of restaurant details page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,8 @@ class RestaurantsController < ApplicationController
   def index
     # TODO: @restaurants
   end
+
+  def show
+    # TODO: @restaurant = ...
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :show]
 end
